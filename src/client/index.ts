d065ca13294645d445ad6453ee1a76ed461243e3
leomosley <leo.mosley2@gmail.com>
2024-08-19 16:38:15 +0100

    chore: deprecate login and home view

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,3 @@
-import Home from "./view/home";
 import DocumentsManager from "./view/document-manager";
-import { LoginView } from "./view/login";
 
-export { Home, DocumentsManager, LoginView };
+export { DocumentsManager };
