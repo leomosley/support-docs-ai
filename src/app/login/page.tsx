e3766e556298800a19aa263c39867fdf1a6a1a3e
leomosley <leo.mosley2@gmail.com>
2024-09-06 11:24:42 +0100

    fix(login): centre login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,16 +9,14 @@ export default async function Login() {
   if (session) return redirect('/home');
 
   return (
-    <main className="bg-grid flex flex-col justify-center">
-      <div className="flex flex-col mt-24 items-center flex-1">
-        <section>
-          <div className="flex flex-col justify-center items-center mx-auto">
-            <h1 className="text-3xl font-bold mb-2">Welcome</h1>
-            <p className="text-gray-600 mb-8">Login to start creating</p>
-            <GoogleLogin />
-          </div>
-        </section>
-      </div>
-    </main>
+    <div className="flex flex-col mt-24 items-center flex-1">
+      <section>
+        <div className="flex flex-col justify-center items-center mx-auto">
+          <h1 className="text-3xl font-bold mb-2">Welcome</h1>
+          <p className="text-gray-600 mb-8">Login to start creating</p>
+          <GoogleLogin />
+        </div>
+      </section>
+    </div>
   );
 };
